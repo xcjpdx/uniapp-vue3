070afea308a74718889b7bf0f16ae9fb9fd764dc
承杰 谢 <1786355989@qq.com>
2024-03-09 02:27:38 +0000

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,3 +1,14 @@
 // 全局常量
-export const BASE_URL = 'http://192.168.1.115:3000';
+
+// H5端
+// #ifdef H5
+export const BASE_URL =
+	process.env.NODE_ENV === 'development' ? '/api' : '请求地址';
+// #endif
+
+// 除了H5端
+// #ifndef H5
+export const BASE_URL = '请求地址';
+// #endif
+
 export const BASE_UPLOAD_URL = '';
