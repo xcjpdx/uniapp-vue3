662ead8f224ef98253dc689bfe39baec38ecd8a6
承杰 谢 <1786355989@qq.com>
2024-10-10 16:07:33 +0800

    更新uviewplus组件库

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import App from './App';
 
 import { getImgUrl } from './utils/index.js';
 
-import uviewPlus from 'uview-plus';
+import uviewPlus, { setConfig } from 'uview-plus';
 
 // pinia
 import { setupStore } from './store/index.js';
@@ -37,8 +37,23 @@ export function createApp() {
 
 	// uview-plus
 	app.use(uviewPlus);
-	// uview-plus 单位配置
-	uni.$u.config.unit = 'rpx';
+	// 需要在app.use(uview-plus)之后执行
+	setConfig({
+		// 修改$u.config对象的属性
+		config: {
+			// 修改默认单位为rpx，相当于执行 uni.$u.config.unit = 'rpx'
+			unit: 'rpx',
+		},
+		// 修改$u.props对象的属性
+		props: {
+			// 修改radio组件的size参数的默认值，相当于执行 uni.$u.props.radio.size = 30
+			radio: {
+				// size: 20
+			},
+			// 其他组件属性配置
+			// ......
+		},
+	});
 
 	// pinia
 	app.use(setupStore());
